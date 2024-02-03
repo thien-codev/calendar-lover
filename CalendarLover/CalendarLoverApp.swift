65b27accfedd7930529ed31c26f71a333602177e
Nguyen Thien <thiennguyen15dt1@gmail.com>
2024-02-03 21:26:16 +0700

    Init project

---
+++ CalendarLover/CalendarLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarLoverApp.swift
+//  CalendarLover
+//
+//  Created by Nguyen Thien on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalendarLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
